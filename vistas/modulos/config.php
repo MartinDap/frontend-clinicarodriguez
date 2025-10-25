b5be602f9c946905af41a0036c40f8fde9992648
renzzoupia <renzoupiachihua3@outlook.com>
2025-10-25 18:51:48 -0500

    Agrego logica de login con token

--- vistas/modulos/config.php
+++ vistas/modulos/config.php
@@ -8,9 +8,9 @@
  */
 
 // URL base del API backend
-define('API_BASE_URL', 'http://localhost:8080/clinica-backend/');
+define('API_BASE_URL', 'http://localhost:8080/api/');
 
 // Header de autenticación (Basic Auth codificado en Base64)
 // NOTA: Este valor debe ser reemplazado por variables de entorno en producción
-define('API_AUTH_HEADER', 'Authorization: Basic JDJhJDA3JGRmaGRmcmV4ZmhnZGZoZGZlcnR0Z2Vwd2RCVk12aVdXRXdLQkZiMjJoTDZNVWtyRk5xRzhPOiQyYSQwNyRkZmhkZnJleGZoZ2RmaGRmZXJ0dGdlZ2N5cFFKZ2JFZ083TGouWGMyNTRnOXYuemtiTGJoeQ==');
+define('API_AUTH_HEADER', 'authorization: Bearer eyJhbGciOiJIUzM4NCJ9.eyJzdWIiOiJtYXJ0aW4xMjMiLCJ1c3VhcmlvSWQiOjMsInJvbCI6IlVTRVIiLCJpYXQiOjE3NjE0MzI0NDIsImV4cCI6MTc2MTUxODg0Mn0.OS2bZ6HneVgiHNTAiw19mdRWvuOQdehNRJ792w9YhWn8v8PHczpDfLoMDDHu1KVE');
 ?>
